8775564aed243df7e7cb532236fe1fbc48a82596
luanaftrindade <luanatrindade.dev@gmail.com>
2024-01-21 17:51:05 +0000

    JSON Model structure to get data

--- webapp/controllers/ProductList.controller.js
+++ webapp/controllers/ProductList.controller.js
@@ -1,8 +1,27 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
-  "use strict";
-  return Controller.extend("sap.ui.demo.walkthrough.ProductList", {
-    onShowProductDetails: function () {
-      alert("Price: 20€");
-    },
-  });
-});
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+  ],
+  function (Controller, MessageToast, JSONModel) {
+    "use strict";
+    return Controller.extend("sap.ui.demo.walkthrough.ProductList", {
+      onInit: function () {
+        // here we can set the data model on the view
+        var data = {
+          recipient: {
+            name: "UI5",
+          },
+        };
+
+        var model = new JSONModel(data);
+        this.getView().setModel(model);
+      },
+      onShowProductDetails: function () {
+        alert("Price: 20€");
+        MessageToast.show("Message from here");
+      },
+    });
+  }
+);
