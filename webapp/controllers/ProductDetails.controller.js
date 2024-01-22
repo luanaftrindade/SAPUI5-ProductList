4fa9e936678d12501bb2fde97342ccfdc2856d15
luanaftrindade <luanatrindade.dev@gmail.com>
2024-01-22 18:51:55 +0000

    Create Dialogs, Fragments and Fragments Callbacks

--- webapp/controllers/ProductDetails.controller.js
+++ webapp/controllers/ProductDetails.controller.js
@@ -1,6 +1,6 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
-  function (Controller, MessageToast) {
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/core/Fragment"],
+  function (Controller, MessageToast, Fragment) {
     "use strict";
     return Controller.extend(
       "sap.ui.demo.walkthrough.controllers.ProductDetails",
@@ -15,6 +15,30 @@ sap.ui.define(
             .getProperty("/recipient/name");
           var message = oBundle.getText("helloMsg", [sRecipient]);
           MessageToast.show(message);
+        },
+        onShowMoreDetails: function () {
+          var view = this.getView();
+
+          // create the dialog lazily
+
+          if (!this.byId("productDetailsDialog")) {
+            // load asynchronous XML fragment
+            Fragment.load({
+              id: view.getId(),
+              name: "sap.ui.demo.walkthrough.views.ProductDetails",
+              controller: this,
+            }).then(function (dialog) {
+              // connect dialog to the root view of this component (including models and lifecycle)
+              view.addDependent(dialog);
+              // and then open it
+              dialog.open();
+            });
+          } else {
+            this.byId("productDetailsDialog").open();
+          }
+        },
+        onCloseDialog: function () {
+          this.byId("productDetailsDialog").close();
         },
       }
     );
